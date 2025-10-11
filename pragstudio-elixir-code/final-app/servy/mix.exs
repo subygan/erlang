0bac0ef867662feafbbe237d6248cd3f4cad1555
Surya Ganesh <suriyaganesh097@gmail.com>
2025-10-11 15:55:38 -0400

    init

---
+++ pragstudio-elixir-code/final-app/servy/mix.exs
@@ -0,0 +1,28 @@
+defmodule Servy.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :servy,
+      description: "A humble HTTP server",
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :eex, :observer, :wx, :runtime_tools],
+      mod: {Servy, []},
+      env: [port: 3000]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [{:poison, "~> 5.0"}]
+  end
+end
---
+++ pragstudio-elixir-code/video-code/28-linking-processes/servy/mix.exs
@@ -0,0 +1,25 @@
+defmodule Servy.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :servy,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :eex, :observer, :wx, :runtime_tools]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [{:poison, "~> 5.0"}]
+  end
+end
